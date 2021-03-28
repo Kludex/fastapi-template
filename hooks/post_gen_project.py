105622e10aba4e8be2f3cc63c4b9cf9542b5de47
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-03-28 11:51:08 -0700

    ✨ Add env file (#2)

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -23,6 +23,10 @@ def remove_poetry():
     os.remove("pyproject.toml")
 
 
+def remove_env_file():
+    os.remove(".env")
+
+
 def main():
     if "{{ cookiecutter.run_server }}" == "CLI":
         remove___main__()
@@ -36,6 +40,8 @@ def main():
         remove_poetry()
     else:
         remove_dev_requirements()
+
+    remove_env_file()
 
 
 if __name__ == "__main__":
